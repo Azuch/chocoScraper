85df566781eb082b5a2aa20b5d7d7ed38449e930
azuch <namlucius@gmail.com>
2024-04-15 01:47:03 +0000

    Done part 2. Please review the item, itemloader, pipeline, because i know you not know it well (we are in hurry, aren't we?).

--- chocolatescraper/items.py
+++ chocolatescraper/items.py
@@ -10,3 +10,9 @@ class ChocolatescraperItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+
+class ChocolateProduct(scrapy.Item):
+   name = scrapy.Field()
+   price = scrapy.Field()
+   url = scrapy.Field()
--- chocolatescraper/spiders/chocolatespider.py
+++ chocolatescraper/spiders/chocolatespider.py
@@ -1,26 +1,29 @@
 import scrapy
- 
+from chocolatescraper.items import ChocolateProduct  
+from chocolatescraper.itemloaders import ChocolateProductLoader
+
 class ChocolateSpider(scrapy.Spider):
 
-   #the name of the spider
-   name = 'chocolatespider'
+  #the name of the spider
+  name = 'chocolatespider'
 
-   #these are the urls that we will start scraping
-   start_urls = ['https://www.chocolate.co.uk/collections/all']
+  #these are the urls that we will start scraping
+  start_urls = ['https://www.chocolate.co.uk/collections/all']
 
-   def parse(self, response):
+  def parse(self, response):
 
-       products = response.css('product-item')
-       for product in products:
-           #here we put the data returned into the format we want to output for our csv or json file
-           yield{
-               'name' : product.css('a.product-item-meta__title::text').get(),
-               'price' : product.css('span.price').get().replace('<span class="price">\n              <span class="visually-hidden">Sale price</span>','').replace('</span>',''),
-               'url' : product.css('div.product-item-meta a').attrib['href'],
-           }
+      products = response.css('product-item')
 
-       next_page = response.css('[rel="next"] ::attr(href)').get()
+      for product in products:
+          chocolate = ChocolateProductLoader(item=ChocolateProduct(), selector=product)
+          chocolate.add_css('name', "a.product-item-meta__title::text")
+          chocolate.add_css('price', 'span.price', re='<span class="price">\n              <span class="visually-hidden">Sale price</span>(.*)</span>')
+          chocolate.add_css('url', 'div.product-item-meta a::attr(href)')
+          yield chocolate.load_item()
 
-       if next_page is not None:
-           next_page_url = 'https://www.chocolate.co.uk' + next_page
-           yield response.follow(next_page_url, callback=self.parse)
+      next_page = response.css('[rel="next"] ::attr(href)').get()
+
+      if next_page is not None:
+          next_page_url = 'https://www.chocolate.co.uk' + next_page
+          yield response.follow(next_page_url, callback=self.parse)
+
